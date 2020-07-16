e028c2748d8c5f76599dd32cdc4ec04312983cf9
Zack Rowe <zack.rowe@pinnacleqi.com>
2020-07-16 05:01:25 -0600

    Create boostrap.sh

---
+++ boostrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+rm -rf ~/.bash_aliases;
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+  
+if [ "$1" == "--remote" ]; then
+  cp work/remote/.bash_aliases ~/.bash_aliases;
+else
+	cp work/local/.bash_aliases ~/.bash_aliases;
+fi;
+
+source ~/.bash_aliases
