69b993fa7b42753491ff83127562230c902d31a2
Zack Rowe <zack.rowe@pinnacleqi.com>
2020-08-07 09:44:58 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,9 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
   
 if [ "$1" == "--remote" ]; then
-  cp work/remote/.bash_aliases ~/.bash_aliases;
+    cp work/remote/.bash_aliases ~/.bash_aliases;
 else
-  cp work/local/.bash_aliases ~/.bash_aliases;
+    cp work/local/.bash_aliases ~/.bash_aliases;
 fi;
 
 . ~/.bash_aliases
