78a6427e1442c759b65604e759eb6f2cee900b7c
Zack Rowe <zack.rowe@pinnacleqi.com>
2020-08-07 12:55:51 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,8 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
+sudo apt-get install -y zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
 cp ./.zshrc ~/.zshrc
